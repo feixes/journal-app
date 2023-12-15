0d5bc28b2afe812f587bf60283668180052f69ee
Xavier Feixas <xavierfeixesguell@gmail.com>
2023-12-15 19:00:35 +0100

    react redux and store created

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { JournalApp } from './JournalApp'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
 
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+
+    <Provider store={store}>
+
+      <BrowserRouter>
+
+        <JournalApp />
+
+      </BrowserRouter>
+
+    </Provider>
+
   </React.StrictMode>
 )
