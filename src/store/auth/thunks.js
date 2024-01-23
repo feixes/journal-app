f36e0af1f7fb3942a3c98ab6d3b6ab36e9c68f7c
Xavier Feixas <xavierfeixesguell@gmail.com>
2024-01-23 21:36:51 +0100

    google sign in configured

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from "../../firebase/providers"
 import { checkingCredentials } from "./authSlice"
 
 
@@ -10,5 +11,7 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = () => {
     return async (dispatch) => {
         dispatch(checkingCredentials())
+        const result = await signInWithGoogle()
+        console.log(result)
     }
 }
